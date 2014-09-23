043589a430a06b932361870946bcfacc3acca53f
Paul Nechifor <paul@nechifor.net>
2014-09-23 20:03:32 +0300

    Fix style and remove unused parts.

--- space-hoarder.py
+++ space-hoarder.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python2
 
 from gi.repository import Gtk
-import sys, os, stat, re
+import os
+import re
+import stat
+import sys
 
 class SpaceHoarderApp(Gtk.Application):
-    ROOT_CONF = "/apps/space-eater"
     COLORS_STRING = "fb4b2d, db6e2c, fb9928, f3c71c, a7c71c, 809921, " + \
             "86c1a1, 7241bc, c53aa9, ff3a90"
     COLORS = None
     FONT_SIZE = 8
     PAD = 1
     SORT = True
-    
+
     def do_activate(self):
         window = SpaceHoarderWindow(self)
         window.show_all()
-        
-    def onPrefChange(self, client, timestamp, entry, *extra):
-        keyName = entry.get_key()[len(S.ROOT_CONF):]
-        
+
 S = SpaceHoarderApp # Alias for less typing of constants.
-        
+
 class SpaceHoarderWindow(Gtk.ApplicationWindow):
     def __init__(self, app):
         Gtk.Window.__init__(self, title="SpaceHoarder", application=app)
@@ -28,7 +27,7 @@ class SpaceHoarderWindow(Gtk.ApplicationWindow):
 
         openBtn = Gtk.Button(label="Open")
         openBtn.connect("clicked", self.onOpenClicked)
-        
+
         self.pathLb = Gtk.Label(single_line_mode=True)
         self.pathLb.set_alignment(0, 0.5)
 
@@ -50,8 +49,8 @@ class SpaceHoarderWindow(Gtk.ApplicationWindow):
 
     def onOpenClicked(self, button):
         dialog = Gtk.FileChooserDialog("Pick a directory", self,
-                Gtk.FileChooserAction.SELECT_FOLDER, 
-                (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, 
+                Gtk.FileChooserAction.SELECT_FOLDER,
+                (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
                 Gtk.STOCK_OPEN, Gtk.ResponseType.ACCEPT))
         dialog.set_local_only(False)
         dialog.set_modal(True)
@@ -70,28 +69,28 @@ class SpaceHoarderWindow(Gtk.ApplicationWindow):
     def onDraw(self, widget, cr):
         rect = self.drawingArea.get_allocation()
         size = rect.width, rect.height
-        
+
         if self.dirModel == None:
             pass # TODO: Draw an empty rectangle.
         else:
             if size != self.lastSize:
                 self.lastSize = size
                 self.fileRects = self.dirModel.getAllRects(size)
-            
+
             self.drawFileRects(cr)
-            
+
         return False
-        
+
     def drawFileRects(self, cr):
         cr.select_font_face("Sans")
         cr.set_font_size(S.FONT_SIZE)
-    
+
         for f in self.fileRects:
             color = S.COLORS[f.colorIndex]
             cr.set_source_rgb(color[0], color[1], color[2])
             cr.rectangle(f.x, f.y, f.w, f.h)
             cr.fill()
-            
+
             if min(f.w, f.h) > S.FONT_SIZE + 2 * S.PAD:
                 cr.rectangle(f.x, f.y, f.w - S.PAD, f.h - S.PAD)
                 cr.clip()
@@ -99,21 +98,21 @@ class SpaceHoarderWindow(Gtk.ApplicationWindow):
                 cr.move_to(f.x + S.PAD, f.y + S.FONT_SIZE + S.PAD)
                 cr.show_text(f.name)
                 cr.reset_clip()
-        
+
 class FileModel:
     def __init__(self, name, size, depth):
         self.name = name
         self.size = size
         self.depth = depth
-    
+
     def getFileRect(self, x, y, w, h, isContainer):
         # If it's smaller than this it can't be drawn.
         if w <= S.PAD and h <= S.PAD:
             return None
-         
+
         return FileRect(x, y, w, h, self.name,
                 self.depth % len(S.COLORS), isContainer)
-                    
+
     def addFileRects(self, rects, x, y, w, h):
         rect = self.getFileRect(x, y, w, h, False)
         if rect != None:
@@ -129,7 +128,7 @@ class DirModel(FileModel):
             dirs = os.listdir(path)
         except OSError:
             return
-            
+
         for f in dirs:
             pathname = os.path.join(path, f)
             try:
@@ -147,10 +146,10 @@ class DirModel(FileModel):
                 newFile = FileModel(f, fileSize, self.depth + 1)
                 self.contains.append(newFile)
                 self.size += fileSize
-        
+
         if S.SORT:
             self.contains = sorted(self.contains, key=lambda x: x.size)
-    
+
     def addFileRects(self, rects, x, y, w, h):
         rect = self.getFileRect(x, y, w, h, True)
         if rect != None:
@@ -158,7 +157,7 @@ class DirModel(FileModel):
             pd = S.PAD
             d = S.FONT_SIZE + 2 * pd
             self.addSplited(rects, self.contains, x+pd, y+d, w-2*pd, h-d-pd)
-            
+
     def addSplited(self, rects, group, x, y, w, h):
         if len(group) == 0 or w <= S.PAD or h <= S.PAD:
             return
@@ -168,16 +167,16 @@ class DirModel(FileModel):
 
         split = [[], []]
         total = [0, 0]
-        which = 0 
-        
+        which = 0
+
         for f in group:
             # Ignore empty files.
             if f.size == 0:
                 continue
-                
+
             split[which].append(f)
             total[which] += f.size
-            
+
             if total[which] >= total[(which + 1) % 2]:
                 which = (which + 1) % 2
 
@@ -193,7 +192,7 @@ class DirModel(FileModel):
         else:
             self.addSplited(rects, split[0], x, y, w, p)
             self.addSplited(rects, split[1], x, y+h-r, w, r)
-        
+
     def getAllRects(self, size):
         rects = []
         self.addFileRects(rects, 0, 0, size[0], size[1])
@@ -206,13 +205,13 @@ class FileRect:
         self.w = w
         self.h = h
         self.name = name
-        self.colorIndex = colorIndex 
+        self.colorIndex = colorIndex
         self.isContainer = isContainer
 
 def hex2tuple(h):
     n = int(h, 16)
     return (n >> 16) / 255.0, ((n >> 8) & 0xFF) / 255.0, (n & 0xFF) / 255.0
-    
+
 if __name__ == "__main__":
     hexColors = re.findall(r"[0-9a-fA-F]{6}", S.COLORS_STRING)
     S.COLORS = [hex2tuple(x) for x in hexColors]
